b7af5ae7c214d4a770bd2b99f53cbd8114ef51f1
krishgupta57 <krishgupta7898@gmail.com>
2026-05-11 21:58:34 +0530

    Switched email to Port 465 SSL for better network compatibility

--- myenv/airbnb/airbnb/settings.py
+++ myenv/airbnb/airbnb/settings.py
@@ -198,8 +198,9 @@ SIMPLE_JWT = {
 # Email Verification Settings (Gmail SMTP)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_TIMEOUT = 5 # Add this: stop waiting after 5 seconds
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
