3e89640a53345abc38ffdbd6f32971c75aaf029c
krishgupta57 <krishgupta7898@gmail.com>
2026-05-11 14:24:40 +0530

    Updated

--- myenv/airbnb/airbnb/wsgi.py
+++ myenv/airbnb/airbnb/wsgi.py
@@ -8,8 +8,13 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 import pymysql
 pymysql.install_as_MySQLdb()
+
+# Add the project directory to the sys.path
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(BASE_DIR)
 
 from django.core.wsgi import get_wsgi_application
 
